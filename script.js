609bd0c79b7e256030cb4ba570404a401338c3d3
Travis Woods <woodstr3313@gmail.com>
2022-02-16 17:59:24 -0500

    started collecting appropriate data from API for current forecast. Updated HTML with Dom inner html method. Added some more JS to start storing and saving search results.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("working")
 var cityInput = document.querySelector("#city-input");
 var btn = document.getElementById("search-button");
 var APIKey = "d9ec5726f4bacb7542a1b30a7c241e6e";
-
+var todaysForcast = document.getElementById("todays-forcast");
 var city;
 //Function for 1st api call to get geographic coordinates. **Read up on event.preventDefault
 function getCoordinates(event) {
@@ -23,20 +23,39 @@ function getCoordinates(event) {
 
 }
 //Function to get forecast with coordinates
-  function getForecast(coord){
-    console.log(coord)
-    console.log(coord.coord)
-    var lat = coord.coord.lat
-    var lon = coord.coord.lon
-    console.log(lat,lon)
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`)
+function getForecast(coord) {
+  console.log(coord)
+  console.log(coord.coord)
+  var lat = coord.coord.lat
+  var lon = coord.coord.lon
+  console.log(lat, lon)
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-    console.log(data)
+      renderForecast(data);
     });
-  }
+}
+function renderForecast(info) {
+  console.log(info)
+  // Create variables for daily template
+  var unixDate = info.current.dt;
+  var dateObject = new Date(unixDate * 1000);
+  var humanDate = dateObject.toLocaleString("en-US", {month: "long", day: "numeric", year:"numeric"});
+  var temp = Math.round((info.current.temp - 273.5) * 1.8 + 32)
+  console.log(temp)
+
+
+  // Create template to inject date into. 
+  var template = `
+    <h2>${city}-${humanDate} </h2>
+    <p> temperature: ${temp} </p>
+  `
+ // Inject template into DOM with inner html method
+ todaysForcast.innerHTML = template  
+
+}
 
 
 //Add event listener for search button.
