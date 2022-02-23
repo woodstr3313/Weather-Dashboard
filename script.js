ea6708ad7962f8c1b0845e4a1118633d87886455
Travis Woods <woodstr3313@gmail.com>
2022-02-23 18:03:47 -0500

    added 5-day forecast function and for loop

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log("working")
 var cityInput = document.querySelector("#city-input");
 var btn = document.getElementById("search-button");
 var APIKey = "d9ec5726f4bacb7542a1b30a7c241e6e";
-var todaysForcast = document.getElementById("todays-forcast");
+var todaysForecast = document.getElementById("todays-forecast");
 var city;
+var fiveDay = document.getElementById("five-day") 
 //Function for 1st api call to get geographic coordinates. **Read up on event.preventDefault
 function getCoordinates(event) {
   //Get user input
@@ -45,18 +46,40 @@ function renderForecast(info) {
   var humanDate = dateObject.toLocaleString("en-US", {month: "long", day: "numeric", year:"numeric"});
   var temp = Math.round((info.current.temp - 273.5) * 1.8 + 32)
   console.log(temp)
-
+  var uvi = info.current.uvi;
+  var humidity = info.current.humidity;
+  var windSpeed = info.current.wind_speed;
+  var icon = info.current.weather[0].icon;
+  console.log(uvi,humidity,windSpeed,icon);
+  
 
   // Create template to inject date into. 
   var template = `
-    <h2>${city}-${humanDate} </h2>
+    <h2>${city}-${humanDate} <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Weather Icon"></h2>
     <p> temperature: ${temp} </p>
+    <p> wind: ${windSpeed}MPH </p>
+    <p> uvi: ${uvi}UV Index: </p>
+    <p> humidity: ${humidity}% </p>
+
   `
+  console.log(todaysForecast);
  // Inject template into DOM with inner html method
- todaysForcast.innerHTML = template  
-
+ todaysForecast.innerHTML = template  
+  renderFiveDay(info)
 }
-
+//Create a function to render five-day forecast. 
+function renderFiveDay(obj){
+  console.log(obj)
+  //Create a four loop that loops over the daily array inside the object. 
+  for(var i = 0; i < 5; i++){
+    console.log(obj.daily[i].dt);
+    var unixDate = obj.daily[i].dt;
+  var dateObject = new Date(unixDate * 1000);
+  var humanDate = dateObject.toLocaleString("en-US", {month: "long", day: "numeric", year:"numeric"});
+  console.log(humanDate)
+  // Create element in for loop and
+  }
+}
 
 //Add event listener for search button.
 btn.addEventListener("click", getCoordinates)
